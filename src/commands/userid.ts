2e1176bb6ab66ae946ce8f7b9fb2617ae96ac3fb
Ronnie <rwaebers@gmail.com>
2022-08-18 19:43:28 +0200

    Added userid command

---
+++ src/commands/userid.ts
@@ -0,0 +1,32 @@
+import type { Command } from '../types/command';
+import { createSimpleEmbed, getUserByMentionOrId } from '../utils';
+
+const userIdCommand: Command = {
+  name: 'userid',
+  description:
+    'Lists the ID of the specified user.\n\nCan also be used inside of a ticket without specifying a user to list the ID of the user that created the ticket.',
+  aliases: ['uid'],
+  usages: ['(user)'],
+  examples: ['@Nuke#9476'],
+  run: async ({ message, args, ticket }) => {
+    let user = await getUserByMentionOrId({ message, args, ticket });
+    if (args.length && !user) {
+      await message.reply({
+        embeds: [
+          createSimpleEmbed('User not found.', {
+            type: 'danger',
+          }),
+        ],
+      });
+      return;
+    }
+
+    if (!user) {
+      user = message.author;
+    }
+
+    await message.reply(`\`${user.id}\``);
+  },
+};
+
+export default userIdCommand;
