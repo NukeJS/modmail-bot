31a62452a781dea280ae44796f3a0f0601cd5e8a
NukeJS <rwaebers@gmail.com>
2022-09-12 21:37:58 +0200

    Add comment

--- src/utils/prefixed.ts
+++ src/utils/prefixed.ts
@@ -1,1 +1,6 @@
+/**
+ * Prefix a string with the bot's prefix.
+ * @param str String to be prefixed
+ * @returns Prefixed string
+ */
 export const prefixed = (str: string | undefined) => `${process.env.PREFIX}${str}`;
